d4a0ad62734969abdd2002ad8f6464ca4c4ef0a8
eoin.mcgonagle <emcgonagle@cayan.com>
2023-12-09 12:56:56 +0000

    Hand comparable

--- 2023/Day7/Hand.cs
+++ 2023/Day7/Hand.cs
@@ -1,13 +1,28 @@
-﻿using AdventOfCode._2023.Day4;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode._2023.Day7;
 
 public class Hand : IComparable<Hand>
 {
-
     private static Regex _validCard = new Regex(@"([2-9TJQKA])\1+");
     private static Regex _invalidCard = new Regex(@"[^2-9TJQKA\s]+");
+    private static Dictionary<char, int> _cardStrength = new ()
+    {
+        {'2', 2},
+        {'3', 3},
+        {'4', 4},
+        {'5', 5},
+        {'6', 6},
+        {'7', 7},
+        {'8', 8},
+        {'9', 9},
+        {'T', 10},
+        {'J', 11},
+        {'Q', 12},
+        {'K', 13},
+        {'Q', 14}
+    };
+
     public Hand(string cards)
     {
         if (string.IsNullOrEmpty(cards))
@@ -35,46 +50,51 @@ public class Hand : IComparable<Hand>
             throw new ArgumentNullException(nameof(other));
         }
 
-        var firstCardLeft = Cards.First();
-        var firstCardRight = other.Cards.First();
-
         var sortedCardsLeft = new string(Cards.OrderBy(x => x).ToArray());
         var sortedCardsRight = new string(other.Cards.OrderBy(x => x).ToArray());
 
         var leftMatches = _validCard.Matches(sortedCardsLeft);
         var rightMatches = _validCard.Matches(sortedCardsRight);
 
-       
+       var leftValue = GetHandValue(leftMatches);
+       var rightValue = GetHandValue(rightMatches);
 
-        //var isNumber = int.TryParse(Id.ToString(), out var value);
+       // Winning hand
+       if (leftValue != rightValue)
+       {
+           return leftValue > rightValue ? 1 : -1;
+       }
 
-        if (ReferenceEquals(this, other))
-            return 0;
-        if (ReferenceEquals(null, other))
-            return 1;
-        return string.Compare(Cards, other.Cards, StringComparison.Ordinal);
+        // It's a draw - who won
+        _cardStrength.TryGetValue(Cards.First(), out var firstCardLeft);
+        _cardStrength.TryGetValue(other.Cards.First(), out var firstCardRight);
+        return firstCardLeft > firstCardRight ? 1 : -1;
     }
 
     private int GetHandValue(MatchCollection matches)
     {
-        if (matches.First().Value.Length == 5)
-        {
-            // Five of a kind
-            return 6;
-        }
-
         switch (matches.Count)
         {
+            case 1 when matches.First().Value.Length == 5:
+                // Five of a kind
+                return 6;
             case 1 when matches.First().Value.Length == 4:
                 // Four of a kind
                 return 5;
             case 2 when matches.Any(m => m.Value.Length == 3):
                 // Full House
                 return 4;
-            case 1 when matches.First().Length == 3:
+            case 1 when matches.First().Value.Length == 3:
                 // Three of a kind
                 return 3;
+            case 2 when matches.All(m => m.Value.Length == 2):
+                // Two pair
+                return 2;
+            case 1 when matches.First().Value.Length == 3:
+                // Two pair
+                return 1;
+            default:
+                return 0;
         }
-
     }
 }
