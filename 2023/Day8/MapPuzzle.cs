7ebce7845025c8a243940e99aaccb6b2c6f9a23b
eoin.mcgonagle <emcgonagle@cayan.com>
2023-12-10 19:56:03 +0000

    Added PuzzleBase

---
+++ 2023/Day8/MapPuzzle.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode._2023.Day8;
+
+public class MapPuzzle : PuzzleBase
+{
+
+    public (int Answer1, int Answer2) GetAnswers(string filePath)
+    {
+        var file = GetFileLines(filePath);
+
+
+    }
+
+}
---
+++ 2023/PuzzleBase.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode._2023;
+
+public class PuzzleBase
+{
+    protected List<string> GetFileLines(string filePath)
+    {
+        if (string.IsNullOrEmpty(filePath))
+        {
+            throw new ArgumentException("Value cannot be null or empty.", nameof(filePath));
+        }
+
+        if (!File.Exists(filePath))
+        {
+            throw new InvalidOperationException($"Input file does not exist. File=\"{filePath}\"");
+        }
+
+        return File.ReadAllLines(filePath).ToList();
+    }
+
+}
